c7cb296713d52022ee7987a89835507ba96e84bc
Абубакир Акматалиев <abubakirakmataliev211@gmail.com>
2023-01-22 20:19:32 -0900

    new file

---
+++ donat.py
@@ -0,0 +1,9 @@
+
+
+
+def donat(x, y, radius):
+    rectMode(CENTER)
+    fill(0, 255, 0)
+    rect(x, y, 4 * radius, 4 * radius)
+    fill(0, 0, 54)
+    rect(x, y, radius / 2, radius / 2)
---
+++ lesson_005_3.pyde
@@ -0,0 +1,10 @@
+from donat import *
+
+size(800, 500)
+background(0, 0, 74)
+
+
+donat(300, 200, 30)
+
+donat(600, 200, 30)
+donat(450, 200, 30)
